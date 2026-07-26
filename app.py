98ef1a8840b5aa3a94fa8a5d1d502417e74e3c32
LinguaBuddy <fadimeselvi888@gmail.com>
2026-07-26 23:54:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,25 +3,23 @@ from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 
 app = Flask(__name__)
-# InfinityFree ve dışarıdan gelen isteklere izin veriyoruz
 CORS(app, resources={r"/*": {"origins": "*"}})
-socketio = SocketIO(app, cors_allowed_origins="*")
+
+# async_mode='threading' ekledik
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
 
 @app.route('/')
 def index():
-    return "MSN Messenger Server Çalışıyor!"
+    return "MSN Messenger Backend Sunucusu Çalışıyor!"
 
-# Kullanıcı bağlandığında
 @socketio.on('connect')
 def handle_connect():
     print("Bir kullanıcı bağlandı!")
 
-# Mesaj geldiğinde herkese anında ilet
 @socketio.on('send_message')
 def handle_message(data):
     emit('receive_message', data, broadcast=True)
 
-# Titreşim (Nudge) geldiğinde herkesin ekranını salla
 @socketio.on('send_nudge')
 def handle_nudge(data):
     emit('receive_nudge', data, broadcast=True)
